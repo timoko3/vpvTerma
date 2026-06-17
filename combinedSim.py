dcb70516be2c49fc9557b0e5418fad5077fba230
timoko3 <freepower27@gmail.com>
2026-06-17 18:27:04 +0300

    good physics work in progress

--- combinedSim.py
+++ combinedSim.py
@@ -12,9 +12,8 @@ dt = 0.05          # Шаг времени
 
 # Изменяемые параметры (стартовые значения)
 T_hot_val = 80.0
-k_heat_val = 0.5
-gamma_val = 1.0
-c_buoy_val = 0.8
+R_mm_val = 5.0        # Радиус капли в миллиметрах
+h_conv_val = 1500.0   # Коэффициент теплоотдачи Вт/(м2*°C)
 
 # --- Начальные условия ---
 y = 0.0
@@ -26,6 +25,8 @@ time_val = 0.0
 max_history = 400
 t_hist, y_hist, T_hist = [], [], []
 P_hist, V_hist = [], []
+current_P = 101.325
+current_V = 0.0
 
 # Переменные для расчета цикла
 W_accum = 0.0
@@ -57,7 +58,7 @@ temp_text = ax_beaker.text(0.1, H - 0.5, '', fontsize=9, bbox=dict(facecolor='wh
 # 2. Центральный график (Высота от времени)
 ax_yt = fig.add_axes([0.22, 0.35, 0.35, 0.55])
 ax_yt.set_title('График y(t)')
-ax_yt.set_ylim(0, H)
+ax_yt.set_ylim(0, H * 100)
 ax_yt.set_xlabel('Время, с')
 ax_yt.grid(True)
 line_yt, = ax_yt.plot([], [], lw=2, color='darkblue')
@@ -74,33 +75,31 @@ cycle_text = ax_cycle.text(0.05, 0.95, '', transform=ax_cycle.transAxes, fontsiz
 
 # --- Ползунки управления (внизу окна) ---
 ax_thot = fig.add_axes([0.15, 0.20, 0.3, 0.03])
-ax_kheat = fig.add_axes([0.15, 0.15, 0.3, 0.03])
-ax_gamma = fig.add_axes([0.60, 0.20, 0.3, 0.03])
-ax_cbuoy = fig.add_axes([0.60, 0.15, 0.3, 0.03])
+ax_rad = fig.add_axes([0.15, 0.15, 0.3, 0.03])
+ax_hconv = fig.add_axes([0.60, 0.20, 0.3, 0.03])
 
-slider_thot = Slider(ax_thot, 'T_дна (°C)', 50.0, 120.0, valinit=T_hot_val)
-slider_kheat = Slider(ax_kheat, 'Теплопередача (k)', 0.1, 2.0, valinit=k_heat_val)
-slider_gamma = Slider(ax_gamma, 'Вязкость (γ)', 0.1, 3.0, valinit=gamma_val)
-slider_cbuoy = Slider(ax_cbuoy, 'Архимед (c)', 0.1, 2.0, valinit=c_buoy_val)
+slider_thot = Slider(ax_thot, 'T_дна (°C)', 50.0, 100.0, valinit=T_hot_val)
+slider_rad = Slider(ax_rad, 'Радиус (мм)', 2.0, 10.0, valinit=R_mm_val)
+slider_hconv = Slider(ax_hconv, 'Теплоотдача (h)', 500.0, 4000.0, valinit=h_conv_val)
 
 def update_params(val):
-    global T_hot_val, k_heat_val, gamma_val, c_buoy_val
+    global T_hot_val, R_mm_val, h_conv_val
     T_hot_val = slider_thot.val
-    k_heat_val = slider_kheat.val
-    gamma_val = slider_gamma.val
-    c_buoy_val = slider_cbuoy.val
+    R_mm_val = slider_rad.val
+    h_conv_val = slider_hconv.val
+    # Обновляем градиент фона
     new_gradient = np.linspace(T_hot_val, T_cold, 100).reshape(100, 1)
     grad_img.set_data(new_gradient)
 
 slider_thot.on_changed(update_params)
-slider_kheat.on_changed(update_params)
-slider_gamma.on_changed(update_params)
-slider_cbuoy.on_changed(update_params)
+slider_rad.on_changed(update_params)
+slider_hconv.on_changed(update_params)
 
 # --- Физический движок ---
 def update_physics():
     global y, v, T_drop, time_val
     global W_accum, Qin_accum, last_W, last_Qin, last_eff, state_in_air
+    global current_P, current_V # <-- ДОБАВЛЕНО
 
     # 1. Температура среды
     T_water = T_hot_val - (T_hot_val - T_cold) * (y / H)
@@ -109,32 +108,33 @@ def update_physics():
     rho_water = 998.0 - 0.4 * (T_water - 20.0)
     rho_aniline = 1022.0 - 1.2 * (T_drop - 20.0)
 
-    # Геометрия капли (пусть радиус будет 5 мм = 0.005 м)
-    R = 0.005
-    V_drop_m3 = (4.0 / 3.0) * 3.14159 * R**3
-    S_cross = 3.14159 * R**2      # Площадь поперечного сечения (для трения)
-    S_surf = 4.0 * 3.14159 * R**2 # Площадь поверхности (для теплообмена)
-    mass = rho_aniline * V_drop_m3
+    # Геометрия капли (Масса постоянна, объем дышит от температуры)
+    R_base = R_mm_val / 1000.0  
+    V_base = (4.0 / 3.0) * 3.14159 * R_base**3
+    mass = 1022.0 * V_base  # Базовая масса при 20°C фиксируется
     
-# 2. Теплообмен (Закон Ньютона-Рихмана)
-    C_p = 2000.0    # Удельная теплоемкость анилина, Дж/(кг*°C)
-    h_conv = 1500.0 # Реальный коэффициент теплоотдачи в воде, Вт/(м2*°C)
+    # ТЕПЕРЬ ОБЪЕМ МЕНЯЕТСЯ ОТ НАГРЕВА!
+    V_drop_m3 = mass / rho_aniline
     
-    # Тепловой поток в Ваттах (Джоулях в секунду) через поверхность
+    # Актуальный радиус для расчета трения и площади теплообмена
+    R_current = (0.75 * V_drop_m3 / 3.14159)**(1/3) 
+    S_cross = 3.14159 * R_current**2      
+    S_surf = 4.0 * 3.14159 * R_current**2
+    
+    # 2. Теплообмен (Закон Ньютона-Рихмана)
+    C_p = 2000.0    
+    h_conv = h_conv_val    
+    
     dQ_dt = h_conv * S_surf * (T_water - T_drop)
-    
-    # На сколько градусов нагреет этот поток нашу массу за время dt
     dT_drop = (dQ_dt / (mass * C_p)) * dt
     T_drop += dT_drop
     
     if dQ_dt > 0:
-        Qin_accum += dQ_dt * dt  # Теперь это честные Джоули подведенного тепла!
+        Qin_accum += dQ_dt * dt  # Честные Джоули
         
     # 3. Динамика (через 2-й закон Ньютона)
-    # Сила = Архимед (вверх) - Тяжесть (вниз)
     F_buoyancy = V_drop_m3 * 9.81 * rho_water
     F_gravity = mass * 9.81
-    
     C_d = 0.47
     F_drag = -np.sign(v) * 0.5 * C_d * rho_water * S_cross * (v**2)
     
@@ -142,22 +142,15 @@ def update_physics():
     a = F_net / mass
     
     v += a * dt  
-    dy = v * dt
-    y += dy      
+    y += v * dt      
     
-    # Считаем работу как площадь P-V петли (A = P * dV)
-    current_P = 100.0 + (H - y) * 0.5  # Давление в кПа
-    dV = 0.002 * dT_drop               # Изменение объема (из формулы V = 1.0 + T_drop * 0.002)
-    W_accum += current_P * dV          # Работа в Джоулях (кПа * Л)
-
     # 4. Граничные условия
-    if y > 0.1:  # Слегка оторвались от дна
+    if y > 0.005:  # <-- ИСПРАВЛЕНО НА 5 мм
         state_in_air = True 
         
     if y <= 0:
         y = 0.0
-        if v < 0:
-            v = 0.0  # Гасим скорость только при ударе сверху вниз
+        if v < 0: v = 0.0 
             
         if state_in_air:
             last_W = W_accum
@@ -169,27 +162,31 @@ def update_physics():
             
     elif y >= H:
         y = H
-        if v > 0:
-            v = 0.0
+        if v > 0: v = 0.0
         
     time_val += dt
     
-# Давление растет с глубиной (H - y), Объем растет с температурой T_drop
-    current_P = 100.0 + (H - y) * 0.5  # Оценочное давление в кПа
-    current_V = 1.0 + T_drop * 0.002   # Оценочный объем (тепловое расширение)
+    # 5. Термодинамика P-V и расчет Работы
+    prev_P = current_P
+    prev_V = current_V
     
+    # Честное давление: Атмосферное (101.3 кПа) + Гидростатика (кПа)
+    current_P = 101.325 + (rho_water * 9.81 * (H - y)) / 1000.0 
+    
+    # Объем капли в миллилитрах (мл = см^3)
+    current_V = V_drop_m3 * 1e6   
+    
+    # Работа: dW = P * dV. кПа * мл = 10^-3 Дж (Миллиджоули)
+    dV_ml = current_V - prev_V
+    dW_Joules = ((current_P + prev_P) / 2.0) * dV_ml * 1e-3
+    W_accum += dW_Joules
+    
+    # Запись в историю
     t_hist.append(time_val)
-    y_hist.append(y)
+    y_hist.append(y * 100) # для графика высоты переводим в см
     T_hist.append(T_drop)
-    P_hist.append(current_P)  # Запись давления
-    V_hist.append(current_V)  # Запись объема
-    
-    if len(t_hist) > max_history:
-        t_hist.pop(0)
-        y_hist.pop(0)
-        T_hist.pop(0)
-        P_hist.pop(0)
-        V_hist.pop(0)
+    P_hist.append(current_P)
+    V_hist.append(current_V)
 
 # --- Анимация ---
 def animate(frame):
@@ -211,14 +208,16 @@ def animate(frame):
     
     # Динамическое масштабирование осей P-V (чтобы цикл всегда был в центре)
     if len(V_hist) > 0:
-        ax_cycle.set_xlim(min(V_hist) - 0.01, max(V_hist) + 0.01)
-        ax_cycle.set_ylim(min(P_hist) - 1, max(P_hist) + 1)
+        v_min, v_max = min(V_hist), max(V_hist)
+        dv = (v_max - v_min) * 0.1 if v_max != v_min else 0.1
+        ax_cycle.set_xlim(v_min - dv, v_max + dv)
+        ax_cycle.set_ylim(101.0, 103.5) # Давление всегда в диапазоне 101.3 - 103.3 кПа
     
     cycle_text.set_text(
         f'Последний цикл:\n'
-        f'Подведено тепла (Q): {last_Qin:.2f} у.е.\n'
-        f'Работа (A): {last_W:.2f} у.е.\n'
-        f'КПД: {last_eff:.1f}%'
+        f'Подведено тепла (Q): {last_Qin:.2f} Дж\n'
+        f'Работа (A): {last_W * 1e6:.2f} мкДж\n'
+        f'КПД: {last_eff:.5f}%'
     )
     
     return drop_plot, temp_text, line_yt, line_cycle, cycle_text
